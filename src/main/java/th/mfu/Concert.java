1081bd98226eed7890bfba2adb1a94649560284b
kamonporrn <122779718+kamonporrn@users.noreply.github.com>
2023-09-11 10:10:44 +0000

    Lab04 6531503094

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,22 +5,46 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
  
     public String getTitle() {
         //TODO: return title
-        return "";
+        return title;
     }
     public String getDescription() {
         //TODO: return description
-        return "";
+        return description;
     }
 
     //TODO: add getters and setters
-
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
